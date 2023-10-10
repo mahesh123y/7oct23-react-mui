078e33e778e02376776e406a517b831d69514bcf
Mahesh Yadav <ymahesh756@gmail.com>
2023-10-10 10:05:33 +0530

    c1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+// 1. Import Area
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Blog from './pages/Blog';
+
+//2. Define Area
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/blog' element={<Blog />} />
+        </Routes>
+      </BrowserRouter>
+
+
+
+    </>
   );
 }
+
+//3. Export Area
+//3.1 Default Export
 
 export default App;
